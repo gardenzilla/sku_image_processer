f29eeac8eed0d9ece3a6dd8abe90182b56896edd
Peter Mezei <mezeipetister@gmail.com>
2021-03-25 10:07:54 +0100

    working..

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,7 @@
 FROM fedora:33
 WORKDIR /usr/local/bin
 COPY ./target/release/sku_imgprocesser_microservice /usr/local/bin/sku_imgprocesser_microservice
+COPY ./gz_sku_img_resize /usr/local/bin/resize_script
 RUN dnf install curl -y
 RUN dnf install ImageMagick -y
 STOPSIGNAL SIGINT
--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,16 @@
 use gzlib::proto::sku_image_processer::sku_image_processer_server::*;
-use sku_imgprocesser_microservice::*;
-use std::env;
 use std::error::Error;
-use tokio::sync::oneshot;
+use std::{env, path::Path};
+use tempdir::TempDir;
+use tokio::{fs::File, sync::oneshot};
+use tokio::{
+  fs::{copy, read_dir},
+  process::Command,
+};
+use tokio::{
+  fs::{create_dir, remove_file},
+  prelude::*,
+};
 use tonic::{transport::Server, Request, Response, Status};
 
 use gzlib::proto;
@@ -20,14 +28,74 @@ impl SkuImageProcesser for SkuImageService {
     &self,
     request: Request<proto::sku_image_processer::AddRequest>,
   ) -> Result<Response<()>, Status> {
-    todo!()
+    let r = request.into_inner();
+    let tmp_dir = TempDir::new("sku_image_temp")?;
+    let image_path = tmp_dir.path().join(r.image_id);
+
+    // 1. Store temp
+    // Create image file
+    let mut image_file = File::create(image_path.clone()).await?;
+    // Write image bytes into it
+    image_file
+      .write_all(&r.image_bytes)
+      .await
+      .map_err(|_| Status::internal("Error while writing new file bytes into file"))?;
+
+    // 2. Resize, produce result images
+    let child = Command::new("./resize_script").arg(image_path).spawn();
+    // Make sure our child succeeded in spawning and process the result
+    let _ = child
+      .map_err(|_| Status::internal("Error while processing and resizing SKU images"))?
+      .await?;
+
+    // 3. Create sku dir if not exist yet
+    create_dir(&format!("data/{}", r.sku))
+      .await
+      .map_err(|_| Status::internal("Error while create sku directory in static space ALL"))?;
+
+    // 4. Copy files to static server
+    let mut d = read_dir(tmp_dir.path().join("output")).await.map_err(|_| {
+      Status::internal(
+        "Error while loading temp dir output folder; during image resize bg process.",
+      )
+    })?;
+
+    while let Some(e) = d.next_entry().await? {
+      copy(
+        e.path(),
+        Path::new("data").join(r.sku.to_string()).join(e.path()),
+      )
+      .await?;
+    }
+
+    Ok(Response::new(()))
   }
 
   async fn remove_image(
     &self,
     request: Request<proto::sku_image_processer::RemoveRequest>,
   ) -> Result<Response<()>, Status> {
-    todo!()
+    // 1. Check if image exist in static server
+    // 2. If yes, remove it
+    let r = request.into_inner();
+
+    let mut d = read_dir(Path::new("data").join(r.sku.to_string()))
+      .await
+      .map_err(|_| {
+        Status::internal("Error while loading SKU static folder; during IMG removal process.")
+      })?;
+
+    while let Some(e) = d.next_entry().await? {
+      if let Some(p) = e.path().to_str() {
+        if p.contains(&format!("_{}", r.image_id)) {
+          remove_file(e.path())
+            .await
+            .map_err(|_| Status::invalid_argument("Hiba a képfájl törlésekor"))?;
+        }
+      }
+    }
+
+    Ok(Response::new(()))
   }
 }
 
